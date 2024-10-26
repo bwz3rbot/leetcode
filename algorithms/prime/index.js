339751de445455e1a426983a8a8aab8810fda0ed
= <bestbotdev9@gmail.com>
2024-10-26 06:40:11 -0700

    isPrime fast

--- algorithms/prime/index.js
+++ algorithms/prime/index.js
@@ -1,4 +1,4 @@
-const isPrime = n => {
+const isPrimeSlow = n => {
 	for (let i = 2; i < n - 1; i++) {
 		for (let j = n; j > 1; j--) {
 			if (i * j === n) return false;
@@ -6,7 +6,25 @@ const isPrime = n => {
 	}
 	return true;
 };
+const isPrime = n => {
+	if (n < 2) return false;
 
-for (let i = 2; i < 100; i++) {
-	if (isPrime(i)) console.log(i);
+	for (let i = 2; i < n; i++) {
+		if (n % i === 0) return false;
+	}
+	return true;
+};
+const isPrimeFast = n => {
+	if (n < 2) return false;
+
+	for (let i = 2; i <= Math.sqrt(n); i++) {
+		if (n % i === 0) return false;
+	}
+	return true;
+};
+
+let count = 0;
+for (let i = 2; i < 1000; i++) {
+	if (isPrimeFast(i)) count++;
 }
+console.log(count);
